ee347eefab8628802161675d46af2a04124cb5bd
t7abhay <abhaytaras18@gmail.com>
2025-05-01 15:37:43 +0530

    fix partitoned error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -63,7 +63,7 @@ export const loginUser = asyncHandler(async (req, res) => {
         const cookieOptions = {
             httpOnly: true,
             secure: isProduction,
-            sameSite: isProduction ? "None" : "Lax",
+            sameSite: "None",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         };
 
